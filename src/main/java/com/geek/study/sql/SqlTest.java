9c60f71e4ddc156f9af2cb69bfdd5ef364786bc0
siyan.liu <siyan.liu@dmall.com>
2021-11-14 19:28:46 +0800

    shardingsphere

--- src/main/java/com/geek/study/sql/SqlTest.java
+++ src/main/java/com/geek/study/sql/SqlTest.java
@@ -1,10 +1,22 @@
 package com.geek.study.sql;
 
+import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSONObject;
+import com.geek.study.sql.criteria.OrderCriteria;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: siyan.liu
@@ -16,7 +28,7 @@ public class SqlTest {
            根据xml文件应用程序Context容器(上下文)
            classpath指配置文件的位置, 起点有java, resources. 写路径时相对这个起点去写
     * */
-    static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config-dao.xml");
+    static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config-dao2.xml");
 
     @Test
     public void test1() {
@@ -25,8 +37,10 @@ public class SqlTest {
         SqlSession sqlSession = (SqlSession) context.getBean("sqlSession");
 
         // 调用 Mapper映射文件里的方法
-        List list = sqlSession.selectList("t1Mapper.selectT1");
-        System.out.println(list);
+        OrderCriteria criteria = new OrderCriteria();
+        criteria.setOrderId(666684391162359808l);
+        List list = sqlSession.selectList("orderMapper.selectOrder",criteria);
+        System.out.println(JSONObject.toJSON(list));
     }
 
 }
---
+++ src/main/java/com/geek/study/sql/criteria/OrderCriteria.java
@@ -0,0 +1,8 @@
+package com.geek.study.sql.criteria;
+
+
+import com.geek.study.sql.entity.OrderEntity;
+
+public class OrderCriteria extends OrderEntity {
+
+}
